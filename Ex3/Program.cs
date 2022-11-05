ae5a38bae8a073e97d4c747299ca14276b789a1d
Elena Rubtsova <villay@mail.ru>
2022-11-06 01:28:53 +0500

    edit Ex3 project

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число от 1 до 7");
+int x=Convert.ToInt32(Console.ReadLine());
+if (x<=5)
+{
+    Console.WriteLine("Будний день");
+}
+if (x==6 || x==7)
+{
+    Console.WriteLine("Выходной день");
+}
